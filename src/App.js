5dcde7d663bd45b403e7d09ee89d9582d010d973
kzielinski96 <kornel.zielinski96@gmail.com>
2020-04-01 18:44:38 +0200

    Changed the hello text value to be fetched from Firebase

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from Speech2Text Project!</p>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    helloText: null
+  };
+
+  componentDidMount() {
+    axios.get("/helloWorld").then(res => {
+      this.setState({
+        helloText: res.data
+      });
+    });
+  }
+  render() {
+    let text = this.state.helloText;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{text}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
