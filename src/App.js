b6c1e5cbcce6ffb838000e8f6b18d8832b3c282f
kzielinski96 <kornel.zielinski96@gmail.com>
2020-04-01 18:08:55 +0200

    Setting up React client for the Speech2Text Web API

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello from Speech2Text Project!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
